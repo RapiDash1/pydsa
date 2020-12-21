bc34b33ed9bf9ec2724066bc9bede9766b0b1e3c
RapiDash <srivatsa.m11@gmail.com>
2020-12-21 09:35:14 +0530

    Added more rotation algorithms

--- Arrays/Basic/rotateArray.py
+++ Arrays/Basic/rotateArray.py
@@ -27,6 +27,7 @@ def jugglingAlgorithm(array, shift):
     
     return array
 
+
 def gcd(a, b):
     if b == 0:
         return a
@@ -34,6 +35,35 @@ def gcd(a, b):
         return gcd(a, a%b)
 
 
+def reverse(array, low, high):
+    a = low
+    b = high
+
+    while a < b:
+        array[a], array[b] = array[b], array[a]
+        a += 1
+        b -= 1
+
+
+def reversalAlgorithm(array, shift):
+    shift %= len(array)
+    reverse(array, 0, len(array)-1)
+    reverse(array, len(array)-shift, len(array)-1)
+    reverse(array, 0, len(array)-1-shift)
+    return array
+
+
+def cyclicRotate(array):
+    initElement = array[0]
+
+    for pos in range(len(array)-1):
+        array[pos] = array[pos+1]
+    
+    array[-1] = initElement
+
+    return array
+
+
 if __name__ == "__main__":
     array = [num+1 for num in range(10)]
-    print(jugglingAlgorithm(array, 2))
+    print(cyclicRotate(array))
